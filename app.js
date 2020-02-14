e958bfdc5dfe8506cbaddac413fff914f1e2b5a4
root <markbvillegas@gmail.com>
2020-02-14 02:33:21 +0000

    modify app.js

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ var indexRouter 	 = require("./routes/index"),
 
 
 //CONNECT TO DATABASE - MONGODB ATLAS
-// mongodb+srv://macvillegas:mac1q2w3e4r5t@cluster0-mspvq.mongodb.net/YelpCamp_v11?retryWrites=true&w=majority
 mongoose.connect(process.env.DATABASEURL, {
 	useUnifiedTopology: true,
 	useNewUrlParser: true,
